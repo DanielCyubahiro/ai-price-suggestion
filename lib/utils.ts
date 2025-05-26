3f3ed6a033eb0999d3d38eb605898847c4537887
Daniel <cyubahirodaniel@gmail.com>
2025-05-26 11:47:39 +0200

    Add comments on utility functions for better code clarity

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,22 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Combines multiple class names into a single string, resolving Tailwind CSS conflicts.
+ *
+ * @param inputs - An array of class values (strings, arrays, or objects).
+ * @returns A string of combined and merged class names.
+ */
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 
+/**
+ * Retrieves an environment variable by its name.
+ * Throws an error if the environment variable is not set.
+ *
+ * @param name - The name of the environment variable.
+ * @returns The value of the environment variable.
+ * @throws Error if the environment variable is missing.
+ */
 export const getEnvVariable = (name: string): string => {
   const value = process.env[name];
   if (!value) {
