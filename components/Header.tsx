9da9e0d9049c7708d0de932e725295e3709307d7
Daniel <cyubahirodaniel@gmail.com>
2025-05-27 10:56:50 +0200

    Update Header styles and improve responsiveness
    Adjusted logo dimensions and navigation spacing for better responsiveness across screen sizes. Updated greeting text with smaller font sizes and enhanced layout scalability. Imported Button component for future use.

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,15 +15,17 @@ export default async function Header() {
         <Link href="/" className="text-2xl font-bold">
           <Image
             src={"/trendies-logo.svg"}
-            alt={"logo"} width={150}
-            height={150}
+            alt={"logo"}
+            width={150}
+            height={50}
+            className="w-32 md:w-40"
           />
         </Link>
-        <nav className="flex items-center space-x-4">
+        <nav className="flex items-center gap-2 sm:gap-3 md:gap-4">
           {session?.user ? (
             <>
-              <p className="text-sm">Welcome, {session.user.name?.split(
-                " ")[0]}!
+              <p className="sm:text-sm xs:block">
+                Hi, {session.user.name?.split(" ")[0]}!
               </p>
               <SignOutButton />
             </>
